bd1df312578ec8aff6da87caa919672c40ffede9
282857341 <49048950+282857341@users.noreply.github.com>
2021-08-19 17:51:58 +0800

    Add files via upload

---
+++ nnunet/experiment_planning/change_batch_size.py
@@ -0,0 +1,10 @@
+from batchgenerators.utilities.file_and_folder_operations import *
+import numpy as np
+
+if __name__ == '__main__':
+    input_file = '/home/xychen/new_transformer/nnUNetFrame/DATASET/nnUNet_preprocessed/Task008_Verse1/nnUNetPlansv2.1_plans_3D.pkl'
+    output_file = '/home/xychen/new_transformer/nnUNetFrame/DATASET/nnUNet_preprocessed/Task008_Verse1/nnUNetPlansv2.1_plans_3D.pkl'
+    a = load_pickle(input_file)
+    #a['plans_per_stage'][0]['batch_size'] = int(np.floor(6 / 9 * a['plans_per_stage'][0]['batch_size']))
+    a['plans_per_stage'][0]['batch_size'] = 4
+    save_pickle(a, output_file)
