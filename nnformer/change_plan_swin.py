b34d0c7200988d2276a396e157db4078f899649c
282857341 <49048950+282857341@users.noreply.github.com>
2021-08-28 14:47:07 +0800

    Update change_plan_swin.py

--- nnformer/change_plan_swin.py
+++ nnformer/change_plan_swin.py
@@ -6,8 +6,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("task", help="task id")
     args = parser.parse_args()
-    print(args.task==2)
-    print(args.task)
     if args.task=='2':
         input_file = '../DATASET/nnFormer_preprocessed/Task002_Synapse/nnFormerPlansv2.1_plans_3D.pkl'
         output_file = '../DATASET/nnFormer_preprocessed/Task002_Synapse/nnFormerPlansv2.1_Synapse_plans_3D.pkl'
@@ -16,10 +14,9 @@ if __name__ == '__main__':
         a['plans_per_stage'][1]['patch_size']=np.array([64,128,128])
         a['plans_per_stage'][1]['pool_op_kernel_sizes']=[[2,2,2],[2,2,2],[2,2,2],[2,2,2]]
         a['plans_per_stage'][1]['conv_kernel_sizes']=[[3,3,3],[3,3,3],[3,3,3],[3,3,3],[3,3,3]]
-        print(output_file)
         save_pickle(a, output_file)
         
-    elif args.task==1:
+    elif args.task=='1':
         input_file = './DATASET/nnFormer_preprocessed/Task001_ACDC/nnFormerPlansv2.1_plans_3D.pkl'
         output_file = './DATASET/nnFormer_preprocessed/Task001_ACDC/nnFormerPlansv2.1_ACDC_plans_3D.pkl'
         a = load_pickle(input_file)
@@ -28,4 +25,4 @@ if __name__ == '__main__':
         a['plans_per_stage'][0]['pool_op_kernel_sizes']=[[1, 2, 2], [1, 2, 2], [2, 2, 2], [2, 2, 2]]
         a['plans_per_stage'][0]['conv_kernel_sizes']=[[3,3,3],[3,3,3],[3,3,3],[3,3,3],[3,3,3]]
         save_pickle(a, output_file)
-    
+    print(output_file)
