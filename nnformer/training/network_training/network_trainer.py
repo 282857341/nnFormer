35b100c2dfb5da61605289415a141214b1622201
jsguo <49048950+282857341@users.noreply.github.com>
2021-09-04 11:11:42 +0800

    Update network_trainer.py

--- nnformer/training/network_training/network_trainer.py
+++ nnformer/training/network_training/network_trainer.py
@@ -122,7 +122,7 @@ class NetworkTrainer(object):
         self.save_every = 50
         self.save_latest_only = False  # if false it will not store/overwrite _latest but separate files each
         # time an intermediate checkpoint is created
-        self.save_intermediate_checkpoints = True  # whether or not to save checkpoint_latest
+        self.save_intermediate_checkpoints = False  # whether or not to save checkpoint_latest
         self.save_best_checkpoint = True  # whether or not to save the best checkpoint according to self.best_val_eval_criterion_MA
         self.save_final_checkpoint = True  # whether or not to save the final checkpoint
 
